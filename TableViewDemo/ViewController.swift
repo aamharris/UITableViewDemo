ac1d6ff4bd5dea6e2c9d84554049b38988bd42dd
Aaron Harris <aamharris@gmail.com>
2019-05-14 12:56:41 -0500

    add base project and basic table view

--- TableViewDemo/ViewController.swift
+++ TableViewDemo/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    var menuItems = ["Turkey Sandwich", "Tuna Sandwich", "BBQ Chicken Sandwich", "Roast Beef", "French Dip"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    //setup number of items per section
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    //called to render each cell in the table view
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.dataSource = self;
+         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel!.text = menuItems[indexPath.row]
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
+        //this will hide extra empty cells
+        self.tableView.tableFooterView = UIView()
+        
+        tableView.dataSource = self
     }
-
-
 }
 
